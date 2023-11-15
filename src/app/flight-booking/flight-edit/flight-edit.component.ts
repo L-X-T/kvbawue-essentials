L-X-T/netcetera-essentials

Extra: Update flight in save edit form

src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
@@ -16,6 +16,7 @@ import { validateRoundTrip } from '../shared/validation/round-trip-validator';
 })
 export class FlightEditComponent implements OnChanges, OnInit, OnDestroy {
   @Input() flight: Flight | undefined | null;
+  @Output() flightChange = new EventEmitter<Flight>();
 
   editForm: FormGroup = this.fb.group({
     id: [
@@ -93,6 +94,10 @@ export class FlightEditComponent implements OnChanges, OnInit, OnDestroy {
   save(): void {
     this.saveFlightSubscription = this.flightService.save(this.editForm.value).subscribe({
       next: (flight) => {
+        console.log('saved flight:', flight);
+
+        this.flightChange.emit(flight);
+
         this.message = 'Success!';
       },
       error: (err: HttpErrorResponse) => {

src/app/flight-booking/flight-search/flight-search.component.ts
@@ -101,4 +101,13 @@ export class FlightSearchComponent implements OnInit, OnDestroy {
   trackById(index: number, flight: Flight): number {
     return flight.id;
   }
+
+  updateFlight(updatedFlight: Flight): void {
+    // console.warn('FlightSearchComponent - updateFlight()');
+    // console.log(updatedFlight);
+
+    this.flights = this.flights.map((flight) => (flight.id === updatedFlight.id ? updatedFlight : flight));
+
+    this.search(); // to update the results
+  }
 }

L-X-T/kvbawue-essentials

src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, Input, OnChanges } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, inject, Input, OnChanges, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
@@ -17,6 +17,7 @@ import { validateRoundTrip } from '../shared/validation/round-trip-validator';
 })
 export class FlightEditComponent implements OnChanges {
   @Input() flight?: Flight | null;
+  @Output() flightChange = new EventEmitter<Flight>();
 
   private readonly destroyRef = inject(DestroyRef);
   private readonly flightService = inject(FlightService);
@@ -92,6 +93,10 @@ export class FlightEditComponent implements OnChanges {
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (flight) => {
+          console.log('saved flight:', flight);
+
+          this.flightChange.emit(flight);
+
           this.message = 'Success!';
         },
         error: (err: HttpErrorResponse) => {

src/app/flight-booking/flight-search/flight-search.component.ts
@@ -127,6 +127,15 @@ export class FlightSearchComponent implements OnInit, OnDestroy {
     this.flights = [{ ...this.flights[0] }, ...this.flights.slice(2)];
   }
 
+  updateFlight(updatedFlight: Flight): void {
+    // console.warn('FlightSearchComponent - updateFlight()');
+    // console.log(updatedFlight);
+
+    this.flights = this.flights.map((flight) => (flight.id === updatedFlight.id ? updatedFlight : flight));
+
+    this.onSearch(); // to update the results
+  }
+
   private markFormGroupDirty(formGroup: FormGroup): void {
     Object.values(formGroup.controls).forEach((c) => c.markAsDirty());
   }